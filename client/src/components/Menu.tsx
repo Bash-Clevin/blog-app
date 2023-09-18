c29bb69088b907e4ac14688cd27efb4cbc24b08d
Bash-Clevin <clevinbash@gmail.com>
2023-09-18 08:22:51 +0300

    feat: Add snackbar menu

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,18 @@
+const Menu = () => {
+  const user = false;
+  return (
+    <div
+      className="bg-black w-[200px] flex flex-col items-start absolute top-12 right-6
+     rounded-md p-4 space-y-4"
+    >
+      {!user && (
+        <h3 className="text-white text-sm hover:text-gray-500">Login</h3>
+      )}
+      {!user && (
+        <h3 className="text-white text-sm hover:text-gray-500">Register</h3>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import { Link } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
+import Menu from './Menu';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const showMenu = () => {
+    setMenu(!menu);
+  };
+
   const userSignedIn: boolean = false;
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
@@ -18,7 +27,7 @@ const Navbar = () => {
           type="text"
         />
       </div>
-      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+      <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4">
         {userSignedIn ? (
           <h3>
             <Link to="/writepost">Write</Link>
@@ -37,6 +46,12 @@ const Navbar = () => {
             <Link to="/register">Register</Link>
           </h3>
         )}
+      </div>
+      <div className="md:hidden text-lg" onClick={showMenu}>
+        <p>
+          <FaBars />
+        </p>
+        {menu && <Menu />}
       </div>
     </div>
   );
